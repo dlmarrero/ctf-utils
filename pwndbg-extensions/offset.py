60ceff17a286f9b01190f9cb6c2abe54f80b67e9
Your Name <you@example.com>
2020-05-13 10:28:50 -0400

    add support for symbol names

--- pwndbg-extensions/offset.py
+++ pwndbg-extensions/offset.py
@@ -1,12 +1,21 @@
 import pwndbg
 import gdb
 import argparse
+import six
 
 parser = argparse.ArgumentParser(description='Find the offset to the base of the module containing the specified address')
-parser.add_argument('address', type=int, help='The address to find the offset to')
+parser.add_argument('address_or_symbol', help='The address or symbol to find the offset to')
 @pwndbg.commands.ArgparsedCommand(parser)
 @pwndbg.commands.OnlyWhenRunning
-def offset(address):
+def offset(address_or_symbol):
+    # Handle either a symbol or an addr (offset puts || offset 0x7fffe000)
+    gdb_val = pwndbg.commands.sloppy_gdb_parse(address_or_symbol)
+    if gdb_val.address:
+        address = int(gdb_val.address)
+    else:
+        address = int(gdb_val)
+
     base_addr = pwndbg.elf.map(address)[0].start
+    print(f'Addr = {hex(address)}')
     print(f'Base = {hex(base_addr)}')
     gdb.execute(f'p/x {hex(address)} - {hex(base_addr)}')
