27e7184e19bee03d17fa9179aa26b26f468b2ceb
Your Name <you@example.com>
2020-05-14 11:12:27 -0400

    move helpers to pwnext module

---
+++ pwnext/__init__.py
@@ -0,0 +1,4 @@
+from pwnext.libcdb import *
+from pwnext.bp import set_bp
+from pwnext.blkrop import BlkROP
+
---
+++ pwnext/bp.py
@@ -0,0 +1,25 @@
+from pwn import gdb
+
+def set_bp(io, *args):
+    gdbscript = ''
+    for breakpoint in args:
+        if type(breakpoint) == str:
+            gdbscript += 'b %s\n' % breakpoint
+            continue
+        elif type(breakpoint) == tuple:
+            addr, cmd_list = breakpoint
+            assert type(cmd_list) == list
+
+            bp_cmds = 'commands\n'
+            bp_cmds += '\n'.join(cmd_list)
+            bp_cmds += '\nend\n'
+        else:
+            addr = breakpoint
+            bp_cmds = ''
+
+        gdbscript += 'bp 0x%x\n%s' % (addr, bp_cmds)
+
+    if args:
+        gdbscript += 'c'
+
+    gdb.attach(io, gdbscript)
